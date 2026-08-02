1fa08874f53e51bb4d0f7b320e6ce4ef9e4865fb
AbdullahShanto <abdurrahman7328@gmail.com>
2026-08-02 20:27:02 +0600

    updated

--- document_loader/tokenSplitter.py
+++ document_loader/tokenSplitter.py
@@ -1,8 +1,11 @@
 import os
 from langchain_community.document_loaders import PyPDFLoader
-
+from langchain_core.prompts import ChatPromptTemplate
 from langchain_text_splitters import TokenTextSplitter
+from dotenv import load_dotenv
+from langchain_mistralai import ChatMistralAI
 
+load_dotenv()
 file_path = os.path.join(os.path.dirname(__file__), 'ch11.pdf')
 data = PyPDFLoader(file_path)
 docs = data.load()
@@ -12,5 +15,17 @@ splitter = TokenTextSplitter(
   chunk_overlap=10
 )
 chunk=splitter.split_documents(docs)
-print(len(chunk))
-print(chunk[5].page_content)
+
+
+template = ChatPromptTemplate.from_messages(
+  [
+    ("system","you are a AI that summarizes the text"),
+    ("human","{chunk}")
+  ]
+)
+model = ChatMistralAI(model = "mistral-small-2603")
+prompt = template.format_messages(chunk="\n\n".join(d.page_content for d in chunk))
+result = model.invoke(prompt)
+print(result)
+# print(len(chunk))
+# print(chunk[5].page_content)
