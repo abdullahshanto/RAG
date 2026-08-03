c0d6dca544cf4851dbd7e943f6329fe82753a987
AbdullahShanto <abdurrahman7328@gmail.com>
2026-08-04 02:47:03 +0600

    created separate file for chroma db

---
+++ db.py
@@ -0,0 +1,28 @@
+
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_mistralai import MistralAIEmbeddings
+from langchain_chroma import Chroma
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+file_path = os.path.join(os.path.dirname(__file__), './document_loader/ch11.pdf')
+data = PyPDFLoader(file_path)
+docs = data.load()
+
+
+splitter = RecursiveCharacterTextSplitter(
+  chunk_size=1000,
+  chunk_overlap=200
+)
+
+chunks = splitter.split_documents(docs)
+
+embeddings= MistralAIEmbeddings()
+vectorStore = Chroma.from_documents(
+  documents = docs,
+  embedding = embeddings,
+  persist_directory = "chroma-db"
+)
