f00e144a11fefafe6724b3fb279d2955f338636f
AbdullahShanto <abdurrahman7328@gmail.com>
2026-09-13 23:05:14 +0600

    updated

--- main.py
+++ main.py
@@ -55,3 +55,18 @@ while True:
     if query == "0":
         break 
     
+    docs = retriever.invoke(query)
+
+    context = "\n\n".join(
+        [doc.page_content for doc in docs]
+    )
+    
+    final_prompt = prompt.invoke({
+        "context" :context,
+        "question": query
+    })
+    
+    response = llm.invoke(final_prompt)
+
+    print(f"\n AI: {response.content}")
+    
