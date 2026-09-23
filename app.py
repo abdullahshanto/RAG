bb5f95805966d5b7e4fd5b15f674df3b4372f8dd
AbdullahShanto <abdurrahman7328@gmail.com>
2026-09-23 22:21:33 +0600

    updated

--- app.py
+++ app.py
@@ -109,21 +109,3 @@ Question:
     st.subheader("Ask Questions From the Book")
 
     query = st.text_input("Enter your question")
-
-    if query:
-
-        docs = retriever.invoke(query)
-
-        context = "\n\n".join(
-            [doc.page_content for doc in docs]
-        )
-
-        final_prompt = prompt.invoke({
-            "context": context,
-            "question": query
-        })
-
-        response = llm.invoke(final_prompt)
-
-        st.write("### AI Answer")
-        st.write(response.content)
