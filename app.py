7d716ff9a0ee58f492c4a6a79c842610d2a90605
AbdullahShanto <abdurrahman7328@gmail.com>
2026-09-25 23:42:04 +0600

    updated

--- app.py
+++ app.py
@@ -118,12 +118,4 @@ Question:
             [doc.page_content for doc in docs]
         )
 
-        final_prompt = prompt.invoke({
-            "context": context,
-            "question": query
-        })
-
-        response = llm.invoke(final_prompt)
-
-        st.write("### AI Answer")
-        st.write(response.content)
+       
